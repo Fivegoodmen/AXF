846a5cc7ce9172abe7d144e8fe0fa8d0fccb84e0
wwwchentnan <wwwchentna@hotmail.com>
2018-09-04 20:16:22 +0800

    Merge branch 'master' of /home/u1804/1805flask/axfflask/AXF with conflicts.

--- app/static/app/home/home.js
+++ app/static/app/home/home.js
@@ -1,28 +1,53 @@
-$(function () {
-    //启动顶部菜单轮播
-    initSwiper()
-//    启动必买轮播
-    initMustBuySwiper()
+function aa() {
+    //创建ajax对象xml请求
+    if(window.XMLHttpRequest){
+        var htx = new XMLHttpRequest();
+    }else{
+        var htx = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    //第二步 确定请求方式 地址 同步/异步
+    htx.open('get','ajax.txt',true);
+    //第三部 发送数据到服务器
+    htx.onreadystatechange = function () {
+        if(htx.readyState == 4 && htx.status == 200){
+            alert(htx.responsseText);
+            $.each(msg.lunbo, function (j,y) {
+                $(".swiper-wrapper").append($('<div class="swiper-slide"><<img src=img/'+y.src+'/></div>div'>))
 
-})
+            });
+        }
 
-function initSwiper() {
-   var mySwiper = new Swiper ('#topSwiper', {
-    autoplay: 5000,//可选选项，自动滑动
-    loop: true,
-    // 如果需要分页器
-    pagination: '.swiper-pagination',
+    }
 
-  })
 }
 
 
-function initMustBuySwiper() {
-   var mySwiper = new Swiper ('#swiperMenu', {
-    slidesPerView: 3,  //一页显示的数量
-    spaceBetween: 5,  //每页之间的间隔
-  })
-}
+
+// $(function () {
+//     //启动顶部菜单轮播
+//     initSwiper()
+// //    启动必买轮播
+//     initMustBuySwiper()
+//
+// })
+//
+// function initSwiper() {
+//    var mySwiper = new Swiper ('#topSwiper', {
+//     autoplay: 5000,//可选选项，自动滑动
+//     loop: true,
+//     // 如果需要分页器
+//     pagination: '.swiper-pagination',
+//
+//   })
+// }
+//
+//
+// function initMustBuySwiper() {
+//    var mySwiper = new Swiper ('#swiperMenu', {
+//     slidesPerView: 3,  //一页显示的数量
+//     spaceBetween: 5,  //每页之间的间隔
+//   })
+// }
 
 
 
--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,3 +1,41 @@
-$(document).ready(function(){
-    document.documentElement.style.fontSize = innerWidth / 10 + "px";
+// $(document).ready(function(){
+//     document.documentElement.style.fontSize = innerWidth / 10 + "px";
+// })
+$(function () {
+    alert(xxxxooooo到此一游)
+    //加载数据
+    $.ajax({
+        url:'/static/HTML/home/',
+        type:'GET',
+        success:function (data) {
+            //添加页码
+            var prev = '<button value="prev">上页</button>';
+            var next = '<button value="next">下页</button>';
+            $('#page').append(prev)
+            for (var j = 1;j <= data.pages; j++){
+                var page = '<button value="'+j+'">'+j+'</button>';
+                $('#page').append(page);
+            }
+            $('#page').append(next);
+        }
+        });
+    //实现根据页码跳转页面
+    $('#page').on('click','button',function () {
+
+        var page = $(this).val();
+        $.ajax({
+            url:'/static/HTML/home/'
+            type:'PUT',
+            data:{
+                'page':page
+            },
+            success: function (data) {
+                if (data.code == 200){
+                    window.location.href='base.html'
+                }
+            }
+        })
+
+    })
+
 })
